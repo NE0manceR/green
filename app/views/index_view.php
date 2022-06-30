c8ed59e446c8fccc260d56b96e0ccdce12fe296b
Olexandr Krynytskyi <neomancer2@gmail.com>
2022-06-30 16:23:47 +0300

    start favorite

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -1,67 +1,70 @@
-<link rel="stylesheet" href="<?= SERVER_URL ?>style/swiper-bundle.min.css">
-<link rel="stylesheet" href="<?= SERVER_URL ?>style/slick.css">
+<link rel="stylesheet"
+      href="<?= SERVER_URL ?>style/swiper-bundle.min.css">
+<link rel="stylesheet"
+      href="<?= SERVER_URL ?>style/slick.css">
 <style>
-  .swiper {
-    width: 100%;
-    height: 100%;
-  }
+.swiper {
+  width: 100%;
+  height: 100%;
+}
 
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: grey;
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: grey;
 
-    /* Center slide text vertically */
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    -webkit-justify-content: center;
-    justify-content: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
 
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 
-  .news .swiper {
-    margin-left: auto;
-    margin-right: auto;
-  }
+.news .swiper {
+  margin-left: auto;
+  margin-right: auto;
+}
 
-  .news .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
-    height: calc((100% - 30px) / 2) !important;
+.news .swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  height: calc((100% - 30px) / 2) !important;
 
-    /* Center slide text vertically */
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    -webkit-justify-content: center;
-    justify-content: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
 
-  .slide {
-    padding: 10px;
-  }
+.slide {
+  padding: 10px;
+}
+
 </style>
 
 <?php $products = $this->load->function_in_alias('catalog', '__get_Products', array('limit' => 20)) ?>
@@ -74,9 +77,10 @@
       <?php if (!empty($all_g)) {
         foreach ($all_g as $item) {
       ?>
-          <li>
-            <a class="main__link" href="<?= SITE_URL . $item->link ?>"><?= $item->name ?></a>
-          </li>
+      <li>
+        <a class="main__link"
+           href="<?= SITE_URL . $item->link ?>"><?= $item->name ?></a>
+      </li>
       <?php }
       } ?>
     </ul>
@@ -86,9 +90,11 @@
     <div class="swiper mySwiper mainSlider">
       <div class="swiper-wrapper">
         <?php for ($i = 0; $i < 10; $i++) { ?>
-          <div class="swiper-slide">
-            <img class="main__slider-img" src="<?= SERVER_URL ?>style/test-imgs/test1.svg" alt="img">
-          </div>
+        <div class="swiper-slide">
+          <img class="main__slider-img"
+               src="<?= SERVER_URL ?>style/test-imgs/test1.svg"
+               alt="img">
+        </div>
         <?php  } ?>
 
       </div>
@@ -101,12 +107,14 @@
 
 </section>
 
-<section id="about" class="about content-block">
+<section id="about"
+         class="about content-block">
   <h2 class="section-title center"><?= $this->text('Про Компанію', 0) ?></h2>
   <div class="about__content">
     <span class="about__years"><?= $this->text('14 років 1000+ об’єктів') ?></span>
     <button class="green-btn icon">
-      <img src="<?= SERVER_URL ?>style/icons/ic_location-white.svg" alt="icon">
+      <img src="<?= SERVER_URL ?>style/icons/ic_location-white.svg"
+           alt="icon">
       <?= $this->text('Карта об’єктів') ?>
     </button>
     <span class="about__title">Your Green Property —
@@ -120,11 +128,16 @@
     <?php $i = 0;
     if (!empty($all_g)) {
       foreach ($all_g as $item) { ?>
-        <a href="<?= SITE_URL . $item->link ?>" class="group__item">
-          <img class="group__img-bcg" src="<?= IMG_PATH . $item->photo ?>" alt="img">
-          <img class="group__ic" src="<?= SERVER_URL ?>style/icons/group/ic_group-<?= $i ?>.svg" alt="img">
-          <span class="group__text"><?= $item->name ?></span>
-        </a>
+    <a href="<?= SITE_URL . $item->link ?>"
+       class="group__item">
+      <img class="group__img-bcg"
+           src="<?= IMG_PATH . $item->photo ?>"
+           alt="img">
+      <img class="group__ic"
+           src="<?= SERVER_URL ?>style/icons/group/ic_group-<?= $i ?>.svg"
+           alt="img">
+      <span class="group__text"><?= $item->name ?></span>
+    </a>
     <?php $i++;
       }
     } ?>
@@ -141,30 +154,37 @@
   </div>
   <div class="slider-container news__items-container">
     <?php foreach ($products as  $product) { ?>
-      <div class="slide">
-        <div class="item-card">
-          <a href="<?= SITE_URL . $product->link ?>" class="item-card__img">
-            <img src="<?= IMG_PATH . $product->sm_photo ?>" alt="img">
-          </a>
-          <h3 class="item-card__title">
-            <?= $product->name ?>
-            <img class="item-card__heart"
-            src="<?= SERVER_URL ?>style/icons/ic_heart-black.svg" onclick="populateStorage(<?= $product->id ?>)"
-            id="like<?= $product->id ?>"
-            >
+    <div class="slide">
+      <div class="item-card">
+        <a id="link-<?= $product->id ?>"
+           href="<?= SITE_URL . $product->link ?>"
+           class="item-card__img">
+          <img id="img-<?= $product->id ?>" src="<?= IMG_PATH . $product->sm_photo ?>"
+               alt="img">
+        </a>
+        <span class="item-card__title">
+          <span id="name-<?= $product->id ?>"><?= $product->name ?></span>
+          <img class="item-card__heart item-card__heart-like"
+               data-status="<?= $product->id ?>"
+               src="<?= SERVER_URL ?>style/icons/ic_heart-black.svg"
+               onclick="populateStorage(<?= $product->id ?>)"
+               id="like<?= $product->id ?>">
 
-            <img class="item-card__heart dislike"
-            src="<?= SERVER_URL ?>style/icons/ic_heart-green.svg" onclick="populateStorage(<?= $product->id ?>)"
-            id="dislike<?= $product->id ?>"
-            >
-          </h3>
-          <span class="item-card__location">
-            <img class="item-card__location-icon" src="<?= SERVER_URL ?>style/icons/ic_location.svg" alt="img">
-            <?= $product->list ?>
-          </span>
-          <span class="item-card__price"><?= number_format($product->price, 0, ' ', ' ') ?> грн</span>
-        </div>
+          <img class="item-card__heart dislike"
+               data-status="<?= $product->id ?>"
+               src="<?= SERVER_URL ?>style/icons/ic_heart-green.svg"
+               onclick="populateStorage(<?= $product->id ?>)"
+               id="dislike<?= $product->id ?>">
+        </span>
+        <span class="item-card__location">
+          <img class="item-card__location-icon"
+               src="<?= SERVER_URL ?>style/icons/ic_location.svg"
+               alt="img">
+          <span id="location-<?= $product->id ?>"><?= $product->list ?></span>
+        </span>
+        <span id="price-<?= $product->id ?>" class="item-card__price"><?= number_format($product->price, 0, ' ', ' ') ?> грн</span>
       </div>
+    </div>
     <?php } ?>
   </div>
 
@@ -175,69 +195,55 @@
 <script src="<?= SERVER_URL ?>js/slick.min.js"></script>
 
 <script>
-  var swiper1 = new Swiper(".mainSlider", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+var swiper1 = new Swiper(".mainSlider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  $('.slider-container').slick({
-    arrow: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    rows: 3,
-    centerPadding: '60px',
-    // autoplay: true,
-    // autoplaySpeed: 4000,
+$('.slider-container').slick({
+  arrow: true,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  rows: 3,
+  centerPadding: '60px',
+  // autoplay: true,
+  // autoplaySpeed: 4000,
 
 
-    responsive: [{
-        breakpoint: 880,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  responsive: [{
+      breakpoint: 880,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       }
-    ],
-    appendArrows: document.querySelector('.news__slider-nav'),
-    nextArrow: "<button type='button' class='news__next-btn'></button>",
-    prevArrow: "<button type='button' class='news__prev-btn'></button>",
-
-
-  });
-
-  function populateStorage(id) {
-    if (localStorage.getItem(id) != 'liked') {
-      localStorage.setItem(id, 'liked');
-      $('#like' + id).hide();
-      $('#dislike' + id).show();
-    } else {
-      localStorage.removeItem(id);
-      $('#like' + id).show();
-      $('#dislike' + id).hide();
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
     }
+  ],
+  appendArrows: document.querySelector('.news__slider-nav'),
+  nextArrow: "<button type='button' class='news__next-btn'></button>",
+  prevArrow: "<button type='button' class='news__prev-btn'></button>",
 
-    console.log(localStorage);
-  }
+
+});
 </script>
--- js/script.js
+++ js/script.js
@@ -26,13 +26,127 @@ function toggle_menu() {
 
 // ========== modal window ==========
 
+let modal_wrap = $('.modal-wrap');
+let modal_bcg = $('.modal-bcg');
+let favorite_list = $('.favorite-list');
+let body_block = $('body');
+let header_favorite = $('.header__favorite');
 
-$('#call, .modal-bcg').on('click', function () {
-  console.log($(this))
-  toggleCallModal();
+$('.call').on('click', function () {
+  toggle_call_modal();
+  toggle_bcg();
+  toggle_body_hide();
 })
 
-function toggleCallModal() {
-  $('.modal-wrap').fadeToggle(100);
-  $('.modal-bcg').fadeToggle(100);
+header_favorite.on('click', function () {
+  toggle_favorite_list();
+  toggle_bcg();
+  toggle_body_hide();
+  header_favorite_active();
+})
+
+function header_favorite_active() {
+  header_favorite.toggleClass('active');
 }
+
+modal_bcg.on('click', function () {
+  modal_wrap.hide();
+  favorite_list.hide();
+  toggle_bcg();
+  toggle_body_hide();
+  header_favorite_active();
+})
+
+function toggle_favorite_list() {
+  favorite_list.fadeToggle(100);
+}
+
+function toggle_body_hide() {
+  body_block.toggleClass('hide');
+}
+
+function toggle_call_modal() {
+  modal_wrap.fadeToggle(100);
+}
+
+function toggle_bcg() {
+  modal_bcg.fadeToggle(100);
+}
+
+
+
+// localstorage
+
+localStorage.clear();
+
+let test = [
+  {
+    name: 'qeqwe',
+    testwww: 'qeqwewq'
+  },
+  {
+    name: 'qeqw123e',
+    testwww: 'qeqwew123q'
+  }
+]
+
+if (!localStorage.hasOwnProperty("favorites")) {
+  localStorage.setItem('favorites', JSON.stringify([]))
+}
+
+
+function populateStorage(id) {
+  let item_link = $(`#link-${id}`).attr('href');
+  let item_photo = $(`#img-${id}`).attr('src');
+  let item_name = $(`#name-${id}`).html();
+  let item_location = $(`#location-${id}`).html();
+  let item_price = $(`#price-${id}`).html();
+
+  let newObj = {
+    id: id,
+    link: item_link,
+    photo: item_photo,
+    name: item_name,
+    location: item_location,
+    price: item_price,
+  }
+
+
+  let copyArray = [git];
+
+  console.log(copyArray)
+
+  if (copyArray.indexOf(item => item.id === newObj.id) === -1) {
+    localStorage.removeItem('favorites')
+    copyArray.push(newObj);
+    console.log(copyArray)
+    localStorage.setItem('favorites', JSON.stringify(newObj));
+    console.log(localStorage);
+
+  }
+
+
+  if (localStorage.getItem(id) != 'liked') {
+    // localStorage.setItem(id, JSON.stringify(test).push());
+    $('#like' + id).hide();
+    $('#dislike' + id).show();
+  } else {
+    localStorage.removeItem(id);
+    $('#like' + id).show();
+    $('#dislike' + id).hide();
+  }
+
+  console.log(JSON.parse(localStorage.getItem('favorites')));
+
+}
+
+
+
+$('.item-card__heart-like').each(function (index) {
+
+  if (localStorage.hasOwnProperty($(this).attr('data-status'))) {
+    $(`.dislike[data-status~=${$(this).attr('data-status')}]`).show();
+    $(this).hide();
+  }
+
+})
