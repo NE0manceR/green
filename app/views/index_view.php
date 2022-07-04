4445b6b7a050b303b3241839e7f9d8b0c112c8c0
Olexandr Krynytskyi <neomancer2@gmail.com>
2022-07-04 11:20:26 +0300

    small fix

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -3,67 +3,67 @@
 <link rel="stylesheet"
       href="<?= SERVER_URL ?>style/slick.css">
 <style>
-.swiper {
-  width: 100%;
-  height: 100%;
-}
+    .swiper {
+        width: 100%;
+        height: 100%;
+    }
 
-.swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: grey;
+    .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: grey;
 
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
-}
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+    }
 
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
+    .swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 
-.news .swiper {
-  margin-left: auto;
-  margin-right: auto;
-}
+    .news .swiper {
+        margin-left: auto;
+        margin-right: auto;
+    }
 
-.news .swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
-  height: calc((100% - 30px) / 2) !important;
+    .news .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        height: calc((100% - 30px) / 2) !important;
 
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
-}
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+    }
 
-.slide {
-  padding: 10px;
-}
+    .slide {
+        padding: 10px;
+    }
 
 </style>
 
@@ -74,28 +74,28 @@
     <h1 class="section-title"><?= $_SESSION['alias']->name ?></h1>
 
     <ul class="main__link-wrap">
-      <?php if (!empty($all_g)) {
-        foreach ($all_g as $item) {
-      ?>
-      <li>
-        <a class="main__link"
-           href="<?= SITE_URL . $item->link ?>"><?= $item->name ?></a>
-      </li>
-      <?php }
-      } ?>
+			<?php if (!empty($all_g)) {
+				foreach ($all_g as $item) {
+					?>
+          <li>
+            <a class="main__link"
+               href="<?= SITE_URL . $item->link ?>"><?= $item->name ?></a>
+          </li>
+				<?php }
+			} ?>
     </ul>
   </div>
 
   <div class="main__slider">
     <div class="swiper mySwiper mainSlider">
       <div class="swiper-wrapper">
-        <?php for ($i = 0; $i < 10; $i++) { ?>
-        <div class="swiper-slide">
-          <img class="main__slider-img"
-               src="<?= SERVER_URL ?>style/test-imgs/test1.svg"
-               alt="img">
-        </div>
-        <?php  } ?>
+				<?php for ($i = 0; $i < 10; $i++) { ?>
+          <div class="swiper-slide">
+            <img class="main__slider-img"
+                 src="<?= SERVER_URL ?>style/test-imgs/test1.svg"
+                 alt="img">
+          </div>
+				<?php } ?>
 
       </div>
       <div class="main__slider-filter"></div>
@@ -104,6 +104,7 @@
       <div class="swiper-button-next"></div>
 
     </div>
+  </div>
 
 </section>
 
@@ -115,7 +116,7 @@
     <button class="green-btn icon">
       <img src="<?= SERVER_URL ?>style/icons/ic_location-white.svg"
            alt="icon">
-      <?= $this->text('Карта об’єктів') ?>
+			<?= $this->text('Карта об’єктів') ?>
     </button>
     <span class="about__title">Your Green Property —
       <?= $this->text('Це агенство нерухомості') ?></span>
@@ -125,22 +126,22 @@
   </div>
 
   <div class="group about__group">
-    <?php $i = 0;
-    if (!empty($all_g)) {
-      foreach ($all_g as $item) { ?>
-    <a href="<?= SITE_URL . $item->link ?>"
-       class="group__item">
-      <img class="group__img-bcg"
-           src="<?= IMG_PATH . $item->photo ?>"
-           alt="img">
-      <img class="group__ic"
-           src="<?= SERVER_URL ?>style/icons/group/ic_group-<?= $i ?>.svg"
-           alt="img">
-      <span class="group__text"><?= $item->name ?></span>
-    </a>
-    <?php $i++;
-      }
-    } ?>
+		<?php $i = 0;
+			if (!empty($all_g)) {
+				foreach ($all_g as $item) { ?>
+          <a href="<?= SITE_URL . $item->link ?>"
+             class="group__item">
+            <img class="group__img-bcg"
+                 src="<?= IMG_PATH . $item->photo ?>"
+                 alt="img">
+            <img class="group__ic"
+                 src="<?= SERVER_URL ?>style/icons/group/ic_group-<?= $i ?>.svg"
+                 alt="img">
+            <span class="group__text"><?= $item->name ?></span>
+          </a>
+					<?php $i++;
+				}
+			} ?>
   </div>
 
 </section>
@@ -153,41 +154,44 @@
     </div>
   </div>
   <div class="slider-container news__items-container">
-    <?php foreach ($products as  $product) { ?>
-    <div class="slide">
-      <div class="item-card">
-        <a id="link-<?= $product->id ?>"
-           href="<?= SITE_URL . $product->link ?>"
-           class="item-card__img">
-          <img id="img-<?= $product->id ?>" src="<?= IMG_PATH . $product->sm_photo ?>"
-               alt="img">
-        </a>
-        <span class="item-card__title">
+		<?php foreach ($products as $product) { ?>
+      <div class="slide">
+        <div class="item-card">
+          <a id="link-<?= $product->id ?>"
+             href="<?= SITE_URL . $product->link ?>"
+             class="item-card__img">
+            <img id="img-<?= $product->id ?>"
+                 src="<?= IMG_PATH . $product->sm_photo ?>"
+                 alt="img">
+          </a>
+          <span class="item-card__title">
           <span id="name-<?= $product->id ?>"><?= $product->name ?></span>
           <img class="item-card__heart item-card__heart-like"
                src="<?= SERVER_URL ?>style/icons/ic_heart-black.svg"
                onclick="populateStorage(<?= $product->id ?>)"
                id="like<?= $product->id ?>"
                data-status="<?= $product->id ?>"
-               >
+          >
 
           <img class="item-card__heart dislike"
                src="<?= SERVER_URL ?>style/icons/ic_heart-green.svg"
                onclick="populateStorage(<?= $product->id ?>)"
                id="dislike<?= $product->id ?>"
                data-status="<?= $product->id ?>"
-               >
+          >
         </span>
-        <span class="item-card__location">
-          <img id="location_ic-<?= $product->id ?>" class="item-card__location-icon"
+          <span class="item-card__location">
+          <img id="location_ic-<?= $product->id ?>"
+               class="item-card__location-icon"
                src="<?= SERVER_URL ?>style/icons/ic_location.svg"
                alt="img">
           <span id="location-<?= $product->id ?>"><?= $product->list ?></span>
         </span>
-        <span id="price-<?= $product->id ?>" class="item-card__price"><?= number_format($product->price, 0, ' ', ' ') ?> грн</span>
+          <span id="price-<?= $product->id ?>"
+                class="item-card__price"><?= number_format($product->price, 0, ' ', ' ') ?> грн</span>
+        </div>
       </div>
-    </div>
-    <?php } ?>
+		<?php } ?>
   </div>
 
   <button class="green-btn"><?= $this->text('Перейти в каталог', 0) ?></button>
@@ -197,55 +201,55 @@
 <script src="<?= SERVER_URL ?>js/slick.min.js"></script>
 
 <script>
-var swiper1 = new Swiper(".mainSlider", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    var swiper1 = new Swiper(".mainSlider", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 
-$('.slider-container').slick({
-  arrow: true,
-  infinite: false,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  rows: 3,
-  centerPadding: '60px',
-  // autoplay: true,
-  // autoplaySpeed: 4000,
-
-
-  responsive: [{
-      breakpoint: 880,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      }
-    },
-    {
-      breakpoint: 560,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ],
-  appendArrows: document.querySelector('.news__slider-nav'),
-  nextArrow: "<button type='button' class='news__next-btn'></button>",
-  prevArrow: "<button type='button' class='news__prev-btn'></button>",
+    $('.slider-container').slick({
+        arrow: true,
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        rows: 3,
+        centerPadding: '60px',
+        // autoplay: true,
+        // autoplaySpeed: 4000,
 
 
-});
+        responsive: [{
+            breakpoint: 880,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    // arrows: false,
+                    // centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ],
+        appendArrows: document.querySelector('.news__slider-nav'),
+        nextArrow: "<button type='button' class='news__next-btn'></button>",
+        prevArrow: "<button type='button' class='news__prev-btn'></button>",
+
+
+    });
 </script>
