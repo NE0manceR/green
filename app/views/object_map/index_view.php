e76422ecba7ae8a627744446b9a8ff2c90893a99
Olexandr Krynytskyi <neomancer2@gmail.com>
2022-07-11 18:18:58 +0300

    map page

--- app/views/object_map/index_view.php
+++ app/views/object_map/index_view.php
@@ -1,6 +1,111 @@
-<!-- <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" type="text/css" media="all" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" type="text/javascript"></script>
+
+<script src="./price_range_script.js" type="text/javascript"></script>
+
+<style>
+  .price-range-block {
+    margin: 60px;
+  }
+
+  .sliderText {
+    width: 40%;
+    margin-bottom: 30px;
+    border-bottom: 2px solid red;
+    padding: 10px 0 10px 0px;
+    font-weight: bold;
+  }
+
+  .ui-slider-horizontal {
+    height: .6em;
+  }
+
+  .ui-slider-horizontal {
+    margin-bottom: 15px;
+    width: 40%;
+  }
+
+  .ui-widget-header {
+    background: #3FE331;
+  }
+
+  .price-range-search {
+    width: 40.5%;
+    background-color: #f9f9f9;
+    border: 1px solid #6e6666;
+    min-width: 40%;
+    display: inline-block;
+    height: 32px;
+    border-radius: 5px;
+    float: left;
+    margin-bottom: 20px;
+    font-size: 16px;
+  }
+
+  .price-range-field {
+    width: 20%;
+    min-width: 16%;
+    background-color: #f9f9f9;
+    border: 1px solid #6e6666;
+    color: black;
+    font-family: myFont;
+    font: normal 14px Arial, Helvetica, sans-serif;
+    border-radius: 5px;
+    height: 26px;
+    padding: 5px;
+  }
+
+  .search-results-block {
+    position: relative;
+    display: block;
+    clear: both;
+  }
+
+  .price-filter-range {
+    width: 90%;
+    margin: 0 auto 15px;
+  }
+
+  .ui-widget-header {
+    background-color: #036A0B;
+  }
+
+  .ui-slider-horizontal .ui-slider-handle {
+    border-radius: 100%;
+    width: 24px;
+    height: 24px;
+    cursor: grab;
+  }
+
+  .ui-slider-horizontal .ui-slider-handle:focus {
+    border: none;
+  }
+
+  .ui-slider-horizontal .ui-slider-handle::after {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto auto;
+    content: "";
+    width: 11px;
+    height: 11px;
+    background-color: #036A0B;
+    border-radius: 100%;
+    outline: none;
+  }
+
+  .ui-slider-horizontal .ui-slider-handle:hover {
+    background-color: #c5c5c5;
+  }
+
+  .ui-slider-horizontal .ui-slider-handle:focus {
+    background-color: #c5c5c5;
+  }
+</style>
+
 
 <section class="map">
   <div class="map__wrap" id="map"></div>
@@ -27,7 +132,17 @@
         </label>
       </div>
       <span class="map__title">Ціна</span>
-     
+      <div id="slider-range" class="price-filter-range" name="rangeInput"></div>
+      <div class="map__input-wrap">
+        <div class="map__input-cover">
+          <span>Мін. ціна</span>
+          <input type="number" min=0 max="9900" oninput="validity.valid||(value='0');" id="min_price" class=" map__range-input" />
+        </div>
+        <div class="map__input-cover">
+          <span>Макс. ціна</span>
+          <input type="number" min=0 max="10000" oninput="validity.valid||(value='10000');" id="max_price" class=" map__range-input" />
+        </div>
+      </div>
     </div>
 
     </div>
@@ -350,4 +465,97 @@
   }
 
   initMap()
+</script>
+
+
+<script>
+  $(document).ready(function() {
+
+    $('#price-range-submit').hide();
+
+    $("#min_price,#max_price").on('change', function() {
+
+      $('#price-range-submit').show();
+
+      var min_price_range = parseInt($("#min_price").val());
+
+      var max_price_range = parseInt($("#max_price").val());
+
+      if (min_price_range > max_price_range) {
+        $('#max_price').val(min_price_range);
+      }
+
+      $("#slider-range").slider({
+        values: [min_price_range, max_price_range]
+      });
+
+    });
+
+
+    $("#min_price,#max_price").on("paste keyup", function() {
+
+      $('#price-range-submit').show();
+
+      var min_price_range = parseInt($("#min_price").val());
+
+      var max_price_range = parseInt($("#max_price").val());
+
+      if (min_price_range == max_price_range) {
+
+        max_price_range = min_price_range + 100;
+
+        $("#min_price").val(min_price_range);
+        $("#max_price").val(max_price_range);
+      }
+
+      $("#slider-range").slider({
+        values: [min_price_range, max_price_range]
+      });
+
+    });
+
+
+    $(function() {
+      $("#slider-range").slider({
+        range: true,
+        orientation: "horizontal",
+        min: 0,
+        max: 20000000,
+        values: [0, 20000000],
+        step: 10000,
+
+        slide: function(event, ui) {
+          if (ui.values[0] == ui.values[1]) {
+            return false;
+          }
+
+          $("#min_price").val(ui.values[0]);
+          $("#max_price").val(ui.values[1]);
+        }
+      });
+
+      $("#min_price").val($("#slider-range").slider("values", 0));
+      $("#max_price").val($("#slider-range").slider("values", 1));
+
+    });
+  });
+
+
+
+
+  // $("#slider-range").slider({
+  //   range: true,
+  //   orientation: "horizontal",
+  //   min: 0,
+  //   max: 200000,
+  //   values: [0, 200000],
+  //   step: 500,
+  //   slide: function(event, ui) {
+  //     if (ui.values[0] == ui.values[1]) {
+  //       return false;
+  //     }
+  //     $("#min_price").val(ui.values[0]);
+  //     $("#max_price").val(ui.values[1]);
+  //   }
+  // });
 </script>
